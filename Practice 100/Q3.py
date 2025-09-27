2cfcdc8c89ddd5673dcf4a1240fd2f0e0fbd14c5
Sahil <sahilsuthar31@gmail.com>
2025-09-27 09:19:08 +0530

    py3

---
+++ Practice 100/Q3.py
@@ -0,0 +1,5 @@
+a,b = map(int, input("Enter 2 values in {32 44} Format: ").split())
+print(f"Your numbers are swapped {b} <-> {a}")    #Temporary swap
+
+#a,b=b,a                                          #Permanent swap.
+#print(f"After swapping: a = {a}, b = {b}")
