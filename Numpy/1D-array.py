b0d38e2d2d37d154697aed3f596ad2e687fde0c1
Sahil <sahilsuthar31@gmail.com>
2025-10-31 10:02:23 +0530

    <Numpy>

---
+++ Numpy/1D-array.py
@@ -0,0 +1,5 @@
+# one dimensional array
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+print(arr)
---
+++ Numpy/multi-D-array.py
@@ -0,0 +1,9 @@
+#multi-dimensional array
+#matrix
+import numpy as np
+
+matrix = np.array([[1,2,3],
+                   [4,5,6]])
+
+print(matrix)
+
